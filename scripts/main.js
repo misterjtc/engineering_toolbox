c0bcaf2eb016d16857c5e4d81bdbcc4214035b3d
jordan.cook <jordan.cook@litens.com>
2019-06-19 18:21:18 -0400

    continued working on the dashboard frameowork

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,28 @@
 $(function(){
+    // Check if the function is ready
     console.log("I'm Ready");
+    // JS for expanding collapsing sidebar tool menus
     $(".expander").on("click", function(){
         console.log('clicking');
         $(".basTools").toggleClass("expanded");
+    });
+    // JS for displaying current date on dashboard
+    n =  new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    d = n.getDate();
+    document.getElementById("today").innerHTML = m + "/" + d + "/" + y;
+    // JS for loading specific content onto the dashboard based on user selection
+    $(".basJointTool").on("click", function(){
+        $("#main").load("wip.html"); 
+    });
+    $(".basStressGen").on("click", function(){
+        $("#main").load("wip.html"); 
+    });
+    $(".basPressTool").on("click", function(){
+        $("#main").load("wip.html"); 
+    });
+    $(".basSpringTool").on("click", function(){
+        $("#main").load("wip.html"); 
     });
 });
