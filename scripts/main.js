f151821351c4079f435b788ee0dd6e34f525d1c0
jordan <jtcook63@gmail.com>
2019-07-01 21:13:04 -0400

    large site improvements including modifying the aside, header and footer code to accomodate scrolling of dashboard apps

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,56 @@
 $(function(){
     // Check if the function is ready
     console.log("I'm Ready");
+    // Load the dashboard on site load
+    $("#main").load("dashboard.html");
+    $(".dashSelector").addClass("litLink"); 
+    // Set margin for main content switch based on height of header
+    var $headerHeight = $("header").height()
+    console.log($headerHeight);
+    $('.centralContent').css('margin-top',$headerHeight);
+    $('.dashboard').css('margin-top',$headerHeight);
+    // JS for collapsing and expanding aside nav menu
+    $(".minify").on("click", function(){
+        var $asideWidth = $('aside').width();
+        console.log($asideWidth);
+        // Close all tool lists and remove expansion formatting
+        $(".basExpander > .expander").removeClass("expanderDown");
+        $(".basExpander").removeClass("lit");
+        $(".genExpander > .expander").removeClass("expanderDown");
+        $(".genExpander").removeClass("lit");
+        $(".alphaExpander > .expander").removeClass("expanderDown");
+        $(".alphaExpander").removeClass("lit");
+        $(".basTools").slideUp();
+        $(".genTools").slideUp();
+        $(".alphaTools").slideUp();
+        // If the nav bar is less than 100px, expand it and do some formatting
+        if ( $asideWidth < 100 ) {
+            $('aside').animate({width: "16%"}, 400, "linear");
+            $('.main').animate({width: "84%"}, 400, "linear");
+            $('aside').removeClass('navMinify');
+            $(".automationBreak").html('<h3 class="uppercase sectionBreak automationSection">Automation</h3>');
+            $(".toolboxBreak").html('<h3 class="uppercase sectionBreak toolboxSection">Toolbox</h3>');
+        // Else if the nav bar is expanded shrink it and do some formatting
+        } else {
+            $('aside').animate({width: "5%"}, 400, "linear");
+            $('.main').animate({width: "95%"}, 400, "linear");
+            $('aside').addClass('navMinify');
+            $(".automationBreak").html('<i class="dotHolder far fa-ellipsis-h"></i>');
+            $(".toolboxBreak").html('<i class="dotHolder far fa-ellipsis-h"></i>');
+        }
+    });
     // JS for expanding collapsing sidebar tool menus
     $(".basExpander").on("click", function(){
-        // $( ".basTools" ).fadeToggle( 1000, "linear" )
-        // $(".basTools").toggleClass("expanded");
         $(".basTools").slideToggle();
         $(".basExpander > .expander").toggleClass("expanderDown");
         $(".basExpander").toggleClass("lit");
     });
     $(".genExpander").on("click", function(){
-        // $(".genTools").toggleClass("expanded");
         $(".genTools").slideToggle();
         $(".genExpander > .expander").toggleClass("expanderDown");
         $(".genExpander").toggleClass("lit");
     });
     $(".alphaExpander").on("click", function(){
-        // $(".alphaTools").toggleClass("expanded");
         $(".alphaTools").slideToggle();
         $(".alphaExpander > .expander").toggleClass("expanderDown");
         $(".alphaExpander").toggleClass("lit");
@@ -28,6 +62,9 @@ $(function(){
     d = n.getDate();
     document.getElementById("today").innerHTML = m + "/" + d + "/" + y;
     // JS for loading specific content onto the dashboard based on user selection
+    $(".dashSelector").on("click", function(){
+        $("#main").load("dashboard.html"); 
+    });
     $(".basJointTool").on("click", function(){
         $("#main").load("jointTool.html"); 
     });
@@ -43,4 +80,16 @@ $(function(){
     $(".tvTool").on("click", function(){
         $("#main").load("tvTool.html"); 
     });
+    // JS for styling tool items when they are selected
+    $(".subLink").on("click", function(){
+        $(".subLink").removeClass("litLink");
+        $(".rootLink").removeClass("litLink");
+        $(this).addClass("litLink"); 
+    });
+    $(".rootLink").on("click", function(){
+        $(".subLink").removeClass("litLink");
+        $(".rootLink").removeClass("litLink");
+        $(this).addClass("litLink"); 
+    });
+
 });
