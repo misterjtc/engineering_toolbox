e97b75c8c2e624851c1307c6913c0b9e41b89749
jordan.cook <jordan.cook@litens.com>
2019-07-09 18:49:56 -0400

    worked on the first two dashboard content sections by adding a random quote generator and popular apps sections. Also curated a bunch of company colors as variables

--- scripts/main.js
+++ scripts/main.js
@@ -100,5 +100,47 @@ $(function(){
         $(".rootLink").removeClass("litLink");
         $(this).addClass("litLink"); 
     });
+    // Random quote generator
+    var colorWheel = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
+    var quoteAuthor = "";
+    var quoteText = "";
+
+    function getQuote() {
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?", function(data) {
+        quoteText = data.quoteText;
+
+        if (data.quoteAuthor) {
+        quoteAuthor = data.quoteAuthor;
+        } else {
+        quoteAuthor = "Anonymous"
+        }
+
+        $('.quoteBox').animate({
+        opacity: 0
+        }, 500, function() {
+        $('.quoteBox').html(quoteText);
+        $(this).animate({
+            opacity: 1
+        }, 500);
+        });
+
+        $('.quoteAuthor').animate({
+        opacity: 0
+        }, 500, function() {
+        $('.quoteAuthor').html(" - " + quoteAuthor);
+        $(this).animate({
+            opacity: 1
+        }, 500);
+        });
+
+        var choice = Math.floor(Math.random() * colorWheel.length);
+
+        $('.quoteText').animate({
+        backgroundColor: colorWheel[choice],
+        color: colorWheel[choice],
+        }, 1000);
+    });
+    }
+    getQuote();
 });
