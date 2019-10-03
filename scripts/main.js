f7ecd406558715742dcd68120eabbf5bdb307fb5
jordan.cook <jtcook63@gmail.com>
2019-10-02 21:49:42 -0400

    started the redesign approach for showing content in the content swticher and mostly finsh the tv calculation tool and chart.js integration

--- scripts/main.js
+++ scripts/main.js
@@ -14,12 +14,12 @@ $(function(){
     // Trying the loader config
     // ********************************************************* */
     // This code display an image shortly while the page loads and then displays the dashbaord
-    function showPage() {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("dashboard").style.opacity = "1";
-        $('#dashboard').fadeIn("slow");
-    }
-    setTimeout(showPage, 1500);
+    // function showPage() {
+    //     document.getElementById("loader").style.display = "none";
+    //     document.getElementById("dashboard").style.opacity = "1";
+    //     $('#dashboard').fadeIn("slow");
+    // }
+    // setTimeout(showPage, 1500);
     // ********************************************************** */
     // JS for collapsing/expanding the nav bar when the user clicks the button
     // ********************************************************** */
@@ -116,38 +116,42 @@ $(function(){
     // Run all of this code when the user clicks on the Dashboard menu selector or the side logo
     // This is my current inefficient and hacky way until I figure out something better.
     $(".dashSelector, .logo").on("click", function(){
-        $("#main").html('<div id="loader" class="loader"> <img src="./assets/jcook_engineering_v2_single_vectorized.png" alt="This is the Litens logo"> </div><div id="dashboard" class="dashboard grid"> <div class="contentItem1 dashBox item size1"> <div class="item-content ronsQuotes"> <div class="contentHeader"> <h3> Inspirational Quotes </h3> </div><div class="quoteMain"> <div class="quotePic"> <img src="./assets/ronsvibes.jpg" alt="These are random inspiration images from unsplash"> </div><div class="quoteText"> <div class="quoteBox"> </div><p class="quoteAuthor"> </p></div></div></div></div><div class="contentItem2 dashBox item size1"> <div class="item-content popularApps"> <div class="contentHeader"> <h3> Engineering Apps </h3> </div><div class="popularApps"> <a href="" class="popApp popApp1" onclick="return false"> <i class="fas fa-wave-sine"></i> <p> Torsional Vibration </p></a> <a href="" class="popApp popApp2" onclick="return false"> <i class="fas fa-cogs"></i> <p> System Natural Freq. </p></a> <a href="" class="popApp popApp3" onclick="return false"> <i class="fas fa-wrench"></i> <p> Bolted Joints </p></a> <a href="" class="popApp popApp4" onclick="return false"> <i class="fas fa-chart-line"></i> <p> Stress-Strain Generator </p></a> <a href="" class="popApp popApp5" onclick="return false"> <i class="far fa-scrubber"></i> <p> Press-fits </p></a> </div></div></div><div class="contentItem3 dashBox autoFeed item size5"> <div class="item-content"> <div class="contentHeader"> <h3> Twitter Feed </h3> </div><div class="feedContent"> <a class="twitter-timeline" href="https://twitter.com/realDonaldTrump?ref_src=twsrc%5Etfw">Tweets by realDonaldTrump</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> </div></div></div><div class="contentItem4 dashBox item size1 katsPics"> <div class="item-content"> <div class="contentHeader"> <h3> Kool Pics </h3> </div><div class="koolHero"> <img src="./assets/test1.jpg" alt="" class="koolCell"> <img src="./assets/test2.jpg" alt="" class="koolCell"> <img src="./assets/test3.jpg" alt="" class="koolCell"> <img src="./assets/test4.jpg" alt="" class="koolCell"> <img src="/assets/test5.jpg" alt="" class="koolCell"> </div></div></div><div class="contentItem5 dashBox item size1 eventList"> <div class="item-content"> <div class="contentHeader"> <h3> Magnificent Events </h3> </div><div class="cooksCalendar"> <iframe src="https://outlook.office365.com/calendar/group/litens.onmicrosoft.com/team_global_pe_hybrid_systems/view/day" frameborder="0"></iframe> </div></div></div><div class="contentItem6 dashBox item size2"> <div class="item-content"> <div class="contentHeader"> <h3> Important Forms </h3> </div><div class="anotherApp"> </div></div></div><div class="contentItem7 dashBox item size1"> <div class="item-content"> <div class="contentHeader"> <h3> Another Important Tool 4 </h3> </div><div class="anotherApp"> </div></div></div><div class="contentItem8 dashBox item size4"> <div class="item-content"> <div class="contentHeader"> <h3> Another Important Tool 5 </h3> </div><div class="anotherApp"> </div></div></div><div class="contentItem9 dashBox item size4"> <div class="item-content"> <div class="contentHeader"> <h3> Another Important Tool 6 </h3> </div><div class="anotherApp"> </div></div></div></div>');
-        $(".headToolSwitch").html('Dashboard');
-        $('.grid').css('margin-bottom', $footerHeight);
-        // Reload the Muuri whenever we reload the dashboard
-        var grid = new Muuri('.grid', {dragEnabled: true});
-        // Reload the quote function whenever we reload the dashbaord
-        getQuote();
-        // Reload the hero picture slider whenever we reload the dashboard
-        $('.koolHero').flickity({
-            pageDots: true,
-            draggable: false,
-            contain: true,
-            autoPlay: 4000,
-            cellSelector: '.koolCell',
-            prevNextButtons: false,
-            wrapAround: true,
-        });
-        // Show the loader image for a few seconds whenever we reload the dashboard to give the content time to load
-        function showPage() {
-            document.getElementById("loader").style.display = "none";
-            document.getElementById("dashboard").style.opacity = "1";
-            $('#dashboard').fadeIn("slow");
-        }
-        setTimeout(showPage, 1500);
+        document.getElementById("dashboard").style.display = "block";
+        document.getElementById("majorTest").style.display = "none";
     });
+    // $(".dashSelector, .logo").on("click", function(){
+    //     $("#main").html('<div id="loader" class="loader"> <img src="./assets/jcook_engineering_v2_single_vectorized.png" alt="This is the Litens logo"> </div><div id="dashboard" class="dashboard grid"> <div class="contentItem1 dashBox item size1"> <div class="item-content ronsQuotes"> <div class="contentHeader"> <h3> Inspirational Quotes </h3> </div><div class="quoteMain"> <div class="quotePic"> <img src="./assets/ronsvibes.jpg" alt="These are random inspiration images from unsplash"> </div><div class="quoteText"> <div class="quoteBox"> </div><p class="quoteAuthor"> </p></div></div></div></div><div class="contentItem2 dashBox item size1"> <div class="item-content popularApps"> <div class="contentHeader"> <h3> Engineering Apps </h3> </div><div class="popularApps"> <a href="" class="popApp popApp1" onclick="return false"> <i class="fas fa-wave-sine"></i> <p> Torsional Vibration </p></a> <a href="" class="popApp popApp2" onclick="return false"> <i class="fas fa-cogs"></i> <p> System Natural Freq. </p></a> <a href="" class="popApp popApp3" onclick="return false"> <i class="fas fa-wrench"></i> <p> Bolted Joints </p></a> <a href="" class="popApp popApp4" onclick="return false"> <i class="fas fa-chart-line"></i> <p> Stress-Strain Generator </p></a> <a href="" class="popApp popApp5" onclick="return false"> <i class="far fa-scrubber"></i> <p> Press-fits </p></a> </div></div></div><div class="contentItem3 dashBox autoFeed item size5"> <div class="item-content"> <div class="contentHeader"> <h3> Twitter Feed </h3> </div><div class="feedContent"> <a class="twitter-timeline" href="https://twitter.com/realDonaldTrump?ref_src=twsrc%5Etfw">Tweets by realDonaldTrump</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> </div></div></div><div class="contentItem4 dashBox item size1 katsPics"> <div class="item-content"> <div class="contentHeader"> <h3> Kool Pics </h3> </div><div class="koolHero"> <img src="./assets/test1.jpg" alt="" class="koolCell"> <img src="./assets/test2.jpg" alt="" class="koolCell"> <img src="./assets/test3.jpg" alt="" class="koolCell"> <img src="./assets/test4.jpg" alt="" class="koolCell"> <img src="/assets/test5.jpg" alt="" class="koolCell"> </div></div></div><div class="contentItem5 dashBox item size1 eventList"> <div class="item-content"> <div class="contentHeader"> <h3> Magnificent Events </h3> </div><div class="cooksCalendar"> <iframe src="https://outlook.office365.com/calendar/group/litens.onmicrosoft.com/team_global_pe_hybrid_systems/view/day" frameborder="0"></iframe> </div></div></div><div class="contentItem6 dashBox item size2"> <div class="item-content"> <div class="contentHeader"> <h3> Important Forms </h3> </div><div class="anotherApp"> </div></div></div><div class="contentItem7 dashBox item size1"> <div class="item-content"> <div class="contentHeader"> <h3> Another Important Tool 4 </h3> </div><div class="anotherApp"> </div></div></div><div class="contentItem8 dashBox item size4"> <div class="item-content"> <div class="contentHeader"> <h3> Another Important Tool 5 </h3> </div><div class="anotherApp"> </div></div></div><div class="contentItem9 dashBox item size4"> <div class="item-content"> <div class="contentHeader"> <h3> Another Important Tool 6 </h3> </div><div class="anotherApp"> </div></div></div></div>');
+    //     $(".headToolSwitch").html('Dashboard');
+    //     $('.grid').css('margin-bottom', $footerHeight);
+    //     // Reload the Muuri whenever we reload the dashboard
+    //     var grid = new Muuri('.grid', {dragEnabled: true});
+    //     // Reload the quote function whenever we reload the dashbaord
+    //     getQuote();
+    //     // Reload the hero picture slider whenever we reload the dashboard
+    //     $('.koolHero').flickity({
+    //         pageDots: true,
+    //         draggable: false,
+    //         contain: true,
+    //         autoPlay: 4000,
+    //         cellSelector: '.koolCell',
+    //         prevNextButtons: false,
+    //         wrapAround: true,
+    //     });
+    //     // Show the loader image for a few seconds whenever we reload the dashboard to give the content time to load
+    //     function showPage() {
+    //         document.getElementById("loader").style.display = "none";
+    //         document.getElementById("dashboard").style.opacity = "1";
+    //         $('#dashboard').fadeIn("slow");
+    //     }
+    //     setTimeout(showPage, 1500);
+    // });
     // ********************************************************** */
     // Loading for various tools
     // ********************************************************** */
-    $(".tvTool").on("click", function(){
-        $("#main").load("tvTool.html");
-        $(".headToolSwitch").html('Torsional Vibration Calculator');
-    });
+    // $(".tvTool").on("click", function(){
+    //     $("#main").load("tvTool.html");
+    //     $(".headToolSwitch").html('Torsional Vibration Calculator');
+    // });
     $(".freqTool").on("click", function(){
         $("#main").load("freqTool.html");
         $(".headToolSwitch").html('System Natural Freq. Calculator');
@@ -182,6 +186,19 @@ $(function(){
         $(".subLink").removeClass("litLink");
         $(".rootLink").removeClass("litLink");
         $(this).addClass("litLink"); 
+    });
+    // THIS IS A TEST
+    $("#tvTool").on("click", function(){
+        $( "#dashboard" ).fadeOut( "slow", function() {
+            // Animation complete
+            console.log("dash fade out ran")
+        });
+        $( "#majorTest" ).fadeIn( "slow", function() {
+            // Animation complete
+            console.log("tvtool fade in ran")
+        });
+        // document.getElementById("dashboard").style.display = "none";
+        // document.getElementById("majorTest").style.display = "block";
     });
     // ********************************************************** */
     // Loading pop-up content when footer links are clicked
@@ -259,6 +276,94 @@ $(function(){
         cellSelector: '.koolCell',
         prevNextButtons: false,
         wrapAround: true,
+    });
+    // Initialize chart.js for tvTool
+    // Define global chart variables
+    tvLabels = [0,90.180,270,360];
+    tvData = [1,0,1,0,1];
+    function tvToolChart(tvData,tvLabels) {
+        var ctx = document.getElementById('tvChart');
+        Chart.defaults.global.defaultFontSize = '19';
+        Chart.defaults.global.defaultFontColor = '#999999';
+        Chart.defaults.global.defaultFontFamily = "'Raleway','Helvetica','sans-serif'";
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: tvLabels,
+                datasets: [{
+                    backgroundColor: 'rgba(65,135,165,0.3)',
+                    borderColor: 'rgba(25,60,130,0.3)',
+                    borderWidth: '3',
+                    label: 'This week',
+                    data: tvData,
+                }]
+            },
+            options: {
+                legend: {
+                    display: false,
+                    labels: {
+                        // fontColor: 'rgb(255,99,132)',
+                        boxWidth: 60,
+                        fontSize: 18,
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'TV Amplitude',
+                    fontSize: '22',
+                },
+                scales: {
+                    xAxes: [{
+                        // type: 'linear',
+                        position: 'bottom',
+                        scaleLabel: {
+                            display: 'true',
+                            labelString: 'Cycles'
+                        }
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: 'true',
+                            labelString: 'Amplitude [°]'
+                        }
+                    }],
+                }
+            }
+        });
+    }
+    // tvTool Calculations
+    $(".tvCalc").on("click", function(){
+        var numCyls = $('.numCyls').val();
+        var rpm = $('.inRPM').val();
+        var tvAmpRad = ((($('.tvAmp').val())/2)*(Math.PI/180));
+        var tvAmp = (($('.tvAmp').val())/2);
+        var fFreq = 0.00833 * rpm * numCyls;
+        var period = 1 / fFreq;
+        var timeX = [];
+        var tvCyc = [1/10,2/10,3/10,4/10,5/10,6/10,7/10,8/10,9/10,1];
+        var sinDisp = [];
+        var maxVel = tvAmpRad * 2 * Math.PI * fFreq;
+        var maxAcc = tvAmpRad * ((2*Math.PI*fFreq)*(2*Math.PI*fFreq));
+        maxVel.toFixed(2);
+        maxAcc.toFixed(2);
+        $('.tvMaxVelOut').text(maxVel.toFixed(2));
+        $('.tvMaxAccOut').text(maxAcc.toFixed(2));
+        for (var j=0;j<10;j++)
+        {
+            timeX[j] = period * j * 0.1;
+        }
+        for (var i=0;i<timeX.length;i++)
+        {
+            sinDisp[i] = tvAmp*Math.cos(2*Math.PI*fFreq*timeX[i]);
+        }
+        console.log(numCyls);
+        console.log(rpm);
+        console.log(tvAmpRad);
+        console.log(fFreq);
+        console.log(period);
+        console.log(timeX);
+        console.log(sinDisp);
+        tvToolChart(sinDisp, tvCyc);
     });
 });
 
